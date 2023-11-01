6e1e8fed2c70ccfbebf486b9b3f601da54a7dab5
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-11-01 19:16:51 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Nav toggle
+const menu = document.querySelector(".navList");
+const humbergBtn = document.querySelector(".humberg-menu-btn");
+
+humbergBtn.addEventListener("click", function toggling() {
+  menu.classList.toggle("showMenu");
+});
