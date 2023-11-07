3b51ac5d01774a3276d880d6d9c508d1631660bc
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-11-07 14:10:42 +0300

    update

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ humbergBtn.addEventListener("click", function toggling() {
   menu.classList.toggle("showMenu");
   console.log("clicked");
 });
+
+// time out
+const launchMessage = document.querySelector(".message");
+
+const today = new Date();
+console.log(launchMessage);
--- dist/app.js
+++
@@ -1,8 +0,0 @@
-// Nav toggle
-const menu = document.querySelector(".navList");
-const humbergBtn = document.querySelector(".humberg-menu-btn");
-
-humbergBtn.addEventListener("click", function toggling() {
-  menu.classList.toggle("showMenu");
-  console.log("clicked");
-});
