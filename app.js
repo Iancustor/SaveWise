97b62ac2eaa2b091f1e0c0f0cc5018d199a869e3
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-11-07 16:36:27 +0300

    update

--- app.js
+++ app.js
@@ -8,7 +8,23 @@ humbergBtn.addEventListener("click", function toggling() {
 });
 
 // time out
-const launchMessage = document.querySelector(".message");
+const launchMes = document.querySelector(".launchMessage");
+const message = document.querySelector(".newMessage");
+setInterval(() => {
+  message.style.backgroundColor = `#${Math.floor(
+    Math.random() * 0xffffff
+  ).toString(16)}`;
+}, 2000);
+setInterval(() => {
+  // Rendering launch message
+  const today = new Date();
+  const hours = today.getHours().toString().padStart(2, "0");
+  const mins = today.getMinutes().toString().padStart(2, "0");
+  const secs = today.getSeconds().toString().padStart(2, "0");
 
-const today = new Date();
-console.log(launchMessage);
+  message.textContent = `Remaining Time  ${hours}:${mins}:${secs}`;
+}, 1000);
+// console.log(hours, mins, secs);
+
+// const launchDate = new Date(`2023-12-31 T 12:00:00`).setTime;
+// console.log(launchDate);
