006f4edca56c4ed0bab5e6247c3e52a155495f68
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-11-26 09:48:18 +0300

    update

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ humbergBtn.addEventListener("click", function toggling() {
   console.log("clicked");
 });
 
+// launch message
+{
+  /* <h2 class="newyear">2023 <br> <span>Happy New Year</span></h2> */
+}
+
 // time out
 const launchMes = document.querySelector(".launchMessage");
 const message = document.querySelector(".newMessage");
@@ -15,16 +20,35 @@ setInterval(() => {
     Math.random() * 0xffffff
   ).toString(16)}`;
 }, 2000);
+
 setInterval(() => {
   // Rendering launch message
-  const today = new Date();
-  const hours = today.getHours().toString().padStart(2, "0");
-  const mins = today.getMinutes().toString().padStart(2, "0");
-  const secs = today.getSeconds().toString().padStart(2, "0");
+  // const today = new Date();
+  // const hours = today.getHours().toString().padStart(2, "0");
+  // const mins = today.getMinutes().toString().padStart(2, "0");
+  // const secs = today.getSeconds().toString().padStart(2, "0");
 
-  message.textContent = `Remaining Time  ${hours}:${mins}:${secs}`;
+  // Launch
+  let launchDate = new Date(`2024-01-01  12:00:00`);
+  // console.log(launchDate);
+
+  let now = new Date(launchDate).getTime();
+  let countDown = new Date().getTime();
+  let period = now - countDown;
+  // console.log(period);
+
+  let day = Math.floor(period / (1000 * 60 * 60 * 24));
+  // console.log(day);
+  let hours = Math.floor((period % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    .toString()
+    .padStart(2, "0");
+  let mins = Math.floor(period % ((1000 * 60 * 60) / (1000 * 60)))
+    .toString()
+    .padStart(2, "0");
+  let secs = Math.floor((period % (1000 * 60)) / 1000)
+    .toString()
+    .padStart(2, "0");
+
+  message.textContent = `${day}Days ${hours}hrs : ${mins}mins : ${secs}secs Remaining `;
 }, 1000);
 // console.log(hours, mins, secs);
-
-// const launchDate = new Date(`2023-12-31 T 12:00:00`).setTime;
-// console.log(launchDate);
