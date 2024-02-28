4b4eab54067258e2d314b84f26dbc0c32363e63f
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-02-28 12:55:01 +0300

    updated time

--- app.js
+++ app.js
@@ -23,32 +23,32 @@ setInterval(() => {
 
 setInterval(() => {
   // Rendering launch message
-  // const today = new Date();
-  // const hours = today.getHours().toString().padStart(2, "0");
-  // const mins = today.getMinutes().toString().padStart(2, "0");
-  // const secs = today.getSeconds().toString().padStart(2, "0");
+  const today = new Date();
+  const hours = today.getHours().toString().padStart(2, "0");
+  const mins = today.getMinutes().toString().padStart(2, "0");
+  const secs = today.getSeconds().toString().padStart(2, "0");
 
   // Launch
-  let launchDate = new Date(`2024-01-01  00:00:00`);
+  // let launchDate = new Date(`2024-01-01  00:00:00`);
   // console.log(launchDate);
 
-  let now = new Date(launchDate).getTime();
-  let countDown = new Date().getTime();
-  let period = now - countDown;
+  // let now = new Date(launchDate).getTime();
+  // let countDown = new Date().getTime();
+  // let period = now - countDown;
   // console.log(period);
 
-  let day = Math.floor(period / (1000 * 60 * 60 * 24));
+  // let day = Math.floor(period / (1000 * 60 * 60 * 24));
   // console.log(day);
-  let hours = Math.floor((period % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    .toString()
-    .padStart(2, "0");
-  let mins = Math.floor(period % ((1000 * 60 * 60) / (1000 * 60 * 1)))
-    .toString()
-    .padStart(2, "0");
-  let secs = Math.floor((period % (1000 * 60)) / 1000)
-    .toString()
-    .padStart(2, "0");
+  // let hours = Math.floor((period % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  //   .toString()
+  //   .padStart(2, "0");
+  // let mins = Math.floor(period % ((1000 * 60 * 60) / (1000 * 60 * 1)))
+  //   .toString()
+  //   .padStart(2, "0");
+  // let secs = Math.floor((period % (1000 * 60)) / 1000)
+  //   .toString()
+  //   .padStart(2, "0");
 
-  message.textContent = `${day}Days ${hours}hrs : ${mins}mins : ${secs}secs Remaining `;
+  message.textContent = `Time ${hours}hrs : ${mins}mins : ${secs}secs `;
 }, 1000);
 // console.log(hours, mins, secs);
